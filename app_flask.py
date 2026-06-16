dba2d4bd444ab7297d0ad02c4859664d899324cb
Krrish Guleria <krrish.guleria.plac@gmail.com>
2026-06-16 23:24:09 +0530

    updated

--- app_flask.py
+++ app_flask.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 import os
 
-API_KEY = os.getenv("OPENWEATHER_API_KEY")
+API_KEY = "77b334c2dda8528f43217f7a408107bc"#os.getenv("OPENWEATHER_API_KEY")
 
 app = Flask(__name__)
 
